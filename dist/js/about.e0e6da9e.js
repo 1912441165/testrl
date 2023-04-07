6b5a090079b49330a202993fce539f0bf92b4d61
1912441165 <1912441165@qq.com>
2023-04-07 09:47:32 +0800

    3 commit

---
+++ dist/js/about.e0e6da9e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktestrl_1"]=self["webpackChunktestrl_1"]||[]).push([[443],{5399:function(t,n,s){s.r(n),s.d(n,{default:function(){return i}});var u=function(){var t=this;t._self._c;return t._m(0)},e=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],l=s(1001),r={},a=(0,l.Z)(r,u,e,!1,null,null,null),i=a.exports}}]);
+//# sourceMappingURL=about.e0e6da9e.js.map
